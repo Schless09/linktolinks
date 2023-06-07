5c6efb22b29eb3ae3f0f9a8995249071077a7f7a
Andrew Schuessler <arschuessler90@gmail.com>
2023-06-07 07:07:17 -0700

    1st commit. react set up

---
+++ Frontend/React/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Vite!</h1>
+      <p>This is a simple component rendered in App.js using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
